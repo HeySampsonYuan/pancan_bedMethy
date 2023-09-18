48b7b7a219d448e4fe96175c0c2f42109b620934
Dongsheng_Yuan <93946356+HeySampsonYuan@users.noreply.github.com>
2023-09-18 11:07:54 +0200

    Update nano_web.py

--- nano_web.py
+++ nano_web.py
@@ -131,7 +131,7 @@ elif option2 == 'bedMethyl':
         
         col1, col2 = st.columns(2)
         col1.metric(label="Number of Input  features", value=len(input_bed))
-        col2.metric(label="Number of Features mapped to Trainingset", num_Features)
+        col2.metric(label="Number of Features mapped to Trainingset",value= num_Features)
        
 
         DM.eval()
