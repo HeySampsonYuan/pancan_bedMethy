9c6b572b5f455681b30d1b7f34fd5dc05f5d0366
Dongsheng_Yuan <93946356+HeySampsonYuan@users.noreply.github.com>
2023-09-19 11:11:22 +0200

    Update nano_web.py

--- nano_web.py
+++ nano_web.py
@@ -51,19 +51,33 @@ print(pd.__version__)
 anno_cpg = pd.read_csv(urlopen('https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=Eb0KSwVCU49Agdru0TPlW34BIFNDMv0Zy640erZGlOQ7AA'))
 #anno_cpg = pickle.load(urlopen("https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=EYHGNr8FHKxIoX_pVXknaAABQrYpq1R47u141o0S-6UaiA"))
 print('anno_cpg done')
-if option1 == 'Pan-cancer_v5i':
-    model_files = pickle.load(urlopen("https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=EYHf66EDVJVPrjPaBimBcocBIGwCFvzx8MHOkrthOYj8CQ"))
-elif option1 == 'Brain Tumor':
-    model_files = pickle.load(urlopen("https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=EZDvisj5GWlGg26ZalOpnx0BZdRGfRfye23mSKmY61shxw"))
 
-model = model_files[0]
-enc =  model_files[1]
-example_bed = model_files[2]
+
+@st.cache
+def load_model(option1):
+    if option1 == 'Pan-cancer_v5i':
+        model_files = pickle.load(urlopen("https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=EYHf66EDVJVPrjPaBimBcocBIGwCFvzx8MHOkrthOYj8CQ"))
+    elif option1 == 'Brain Tumor':
+        model_files = pickle.load(urlopen("https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=EZDvisj5GWlGg26ZalOpnx0BZdRGfRfye23mSKmY61shxw"))
+    model = model_files[0]
+    enc =  model_files[1]
+    example_bed = model_files[2]
+    print('load model')
+    last_key = list(model)[-1]
+    DM = NN_classifier(model[last_key].size()[1],model[last_key].size()[0])
+    DM.load_state_dict(model)
+    DM.to(device)
+    return DM,enc,example_bed
+
+
+'''
 last_key = list(model)[-1]
 DM = NN_classifier(model[last_key].size()[1],model[last_key].size()[0])
 DM.load_state_dict(model)
 DM.to(device)
-print(DM)
+'''
+DM,enc,example_bed = load_model(option1)
+
 def match_bs(anno_cpg,input_bed):
     res_bed = anno_cpg.merge(input_bed,how='left',on=["CpG_chrm", "CpG_beg"])
     res_bed = res_bed[~res_bed['beta_values'].isna()]
