e8c046c78b6c7d83c3468784f45d8dd8bbca1e91
Dongsheng_Yuan <93946356+HeySampsonYuan@users.noreply.github.com>
2023-09-20 14:12:03 +0200

    Update nano_web.py

--- nano_web.py
+++ nano_web.py
@@ -185,7 +185,7 @@ elif option2 == 'idats':
         with open(os.path.join("tempDir",uploaded_file[1].name),"wb") as f:
             f.write(uploaded_file[1].getbuffer())
         sample_sheet = methylprep.get_sample_sheet('tempDir/', filepath=None)
-
+        st.write(os.system('ls tempDir/'))
         data_containers = methylprep.run_pipeline('tempDir/', export=False,betas=True,array_type=None, manifest_filepath=None, sample_sheet_filepath=None, sample_names=None)
         data_containers = data_containers[~data_containers.iloc[:,0].isna()]
         data_containers['probe_id']=data_containers.index
