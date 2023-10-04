dad0da2bca0b164c81e0f74533e957ebf947a81c
Dongsheng_Yuan <93946356+HeySampsonYuan@users.noreply.github.com>
2023-10-04 11:44:01 +0200

    Update nano_web.py

--- nano_web.py
+++ nano_web.py
@@ -62,7 +62,7 @@ def load_model(option1):
     if option1 == 'Pan-cancer_v5i':
         model_files = pickle.load(urlopen("https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=EYHf66EDVJVPrjPaBimBcocBIGwCFvzx8MHOkrthOYj8CQ"))
     elif option1 == 'Brain Tumor':
-        model_files = pickle.load(urlopen('https://charitede-my.sharepoint.com/:u:/g/personal/dongsheng_yuan_charite_de/EZDvisj5GWlGg26ZalOpnx0BZdRGfRfye23mSKmY61shxw?e=LOCf7A'))
+        model_files = pickle.load(urlopen('https://charitede-my.sharepoint.com/personal/dongsheng_yuan_charite_de/_layouts/52/download.aspx?share=EZDvisj5GWlGg26ZalOpnx0BZdRGfRfye23mSKmY61shxw'))
     model = model_files[0]
     enc =  model_files[1]
     example_bed = model_files[2]
